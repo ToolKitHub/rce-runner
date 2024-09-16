fc2c1f8292f420abb8bb259f4d5c65c2802b6bbd
xosnrdev <successxodev@gmail.com>
2024-09-16 07:51:15 -0400

    chore: Bump version to 1.2.1

--- Cargo.nix
+++ Cargo.nix
@@ -139,7 +139,7 @@ rec {
       };
       "rce-runner" = rec {
         crateName = "rce-runner";
-        version = "1.2.0";
+        version = "1.2.1";
         edition = "2021";
         crateBin = [
           {
