9a16005eaa4cc768d802df34c0e04eda2e35e9cd
xosnrdev <successxodev@gmail.com>
2024-11-02 07:55:22 -0400

    chore(cleanup): monthly maintenance and refactoring

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-JSON=$(cat <<-END
-{
-    "language": "python",
-    "files": [
-        {
-            "name": "main.py",
-            "content": "print(42)"
-        }
-    ]
-}
-END)
+JSON=$(
+	cat <<-END
+		{
+		    "language": "python",
+		    "files": [
+		        {
+		            "name": "main.py",
+		            "content": "print(42)"
+		        }
+		    ]
+		}
+	END
+)
 
-echo $JSON | cargo run
+echo "$JSON" | cargo run
